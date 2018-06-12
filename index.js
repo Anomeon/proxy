3f42ee78366ea68406d2309056950325595a4bca
Daniyal Gabitov <danuy@list.ru>
2018-06-12 22:54:01 +0300

    Set cors for all requests

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const proxy = require('express-http-proxy');
 const cors = require('cors');
 const app = new Express();
 
-app.options('*', cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use('*', cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use('/', proxy('http://localhost:8080'));
 app.listen(3006, () => console.log(`EXPRESS Server listening on 3006`));
