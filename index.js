c88aede3367c96bc7e6b272c5c4659ccc3ca5bb1
Daniyal Gabitov <danuy@list.ru>
2018-06-24 12:56:38 +0300

    Hardly set allow-origin header

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const proxy = require('express-http-proxy');
 const cors = require('cors');
 const app = new Express();
 
-app.use('*', cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use('/', proxy('http://localhost:8080'));
+const origin = 'http://localhost:3000';
+
+app.use('*', cors({ origin, credentials: true }));
+app.use('/', proxy('http://localhost:8080', {
+  userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
+    headers['access-control-allow-origin'] = origin;
+    return headers;
+  }
+}));
 app.listen(3006, () => console.log(`EXPRESS Server listening on 3006`));
