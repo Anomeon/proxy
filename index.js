ca7a33c1c20e093ebacd8236c101dfe2f9acddc0
Daniyal Gabitov <danuy@list.ru>
2019-02-11 10:54:21 +0300

    Fix index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cors = require('cors');
 
 const app = new Express();
 const origin = 'http://localhost:3000';
+const proxyUrl = 'http://localhost:8080';
 const listeningPort = 3006;
 
 app.use('*', cors({ origin, credentials: true }));
-app.use('/', proxy('http://localhost:8080', {
+app.use('/', proxy(proxyUrl, {
   userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
     headers['access-control-allow-origin'] = origin;
     return headers;
