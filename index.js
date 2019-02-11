52ffb80c34fb7416443246467cb62e50bcc4f6f8
Roman Gafiatullin <gafiatullin.roman@gmail.com>
2019-02-11 12:43:18 +0300

    Add use of .env and start script

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+require('dotenv').load();
 const Express = require('express');
 const proxy = require('express-http-proxy');
 const cors = require('cors');
-
 const app = new Express();
-const origin = 'http://localhost:3000';
-const proxyUrl = 'http://localhost:8080';
-const listeningPort = 3006;
 
-app.use('*', cors({ origin, credentials: true }));
-app.use('/', proxy(proxyUrl, {
+const DEFAULT_ORIGIN = 'http://localhost:3000';
+const DEFAULT_PORT = '8080';
+
+const {
+  PORT = DEFAULT_PORT,
+  PROXY_URL,
+  ORIGIN = DEFAULT_ORIGIN
+} = process.env;
+
+app.use('*', cors({ origin: ORIGIN, credentials: true }));
+app.use('/', proxy(PROXY_URL, {
   userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
-    headers['access-control-allow-origin'] = origin;
+    headers['access-control-allow-origin'] = ORIGIN;
     return headers;
   }
 }));
-app.listen(listeningPort, () => console.log(`EXPRESS Server listening on ${listeningPort}`));
 
+app.listen(PORT, () => console.log(`EXPRESS Server listening on ${PORT}`));
