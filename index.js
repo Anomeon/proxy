7bbbd240b6cf804358ee5493506c1d656fdfab88
Daniyal Gabitov <danuy@list.ru>
2019-10-01 17:45:13 +0300

    Added EXTRA_PROXY_MAPPING_RULES

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ const {
     PORT = DEFAULT_PORT,
     PROXY_URL,
     COOKIE_DOMAIN,
-    ORIGIN = DEFAULT_ORIGIN
+    ORIGIN = DEFAULT_ORIGIN,
 } = process.env;
 
+const EXTRA_PROXY_MAPPING_RULES = process.env.EXTRA_PROXY_MAPPING_RULES
+  ? JSON.parse(process.env.EXTRA_PROXY_MAPPING_RULES)
+  : [];
+
 app.use('*', cors({ origin: ORIGIN, credentials: true }));
-app.use('/', proxy(PROXY_URL, {
-  userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
-    headers['access-control-allow-origin'] = ORIGIN;
-    if (headers["set-cookie"]) {
-      headers["set-cookie"] = headers["set-cookie"].map(element => element.replace(`domain=${COOKIE_DOMAIN}`, 'domain=localhost'));
-    }
-    return headers;
+
+app.use('/', proxy(
+  (request) => {
+    const proxyUrlFromRule = EXTRA_PROXY_MAPPING_RULES.reduce((acc, rule) => {
+      if (acc === '' && request.url.indexOf(rule.requestUrlPart) !== -1) {
+        return rule.proxyUrl;
+      }
+      return acc;
+    }, '');
+    const currentProxyUrl = proxyUrlFromRule === '' ? PROXY_URL : proxyUrlFromRule;
+    console.log(`url: ${currentProxyUrl}${request.url}`);
+    return currentProxyUrl;
+  },
+  {
+    userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
+      headers['access-control-allow-origin'] = ORIGIN;
+      if (headers["set-cookie"]) {
+        headers["set-cookie"] = headers["set-cookie"].map(element => element.replace(`domain=${COOKIE_DOMAIN}`, 'domain=localhost'));
+      }
+      return headers;
+    },
   }
-}));
-
+));
 app.listen(PORT, () => console.log(`Proxying ${PROXY_URL} to localhost:${PORT} with origin ${ORIGIN}`));
-
